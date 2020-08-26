a5e8f52db0aacd618aaad083b9cd7970184cedc0
miguelgutierrez2017 <miguelgutierrez.a2017@gmail.com>
2020-08-25 21:45:02 -0700

    Initial Commit

---
+++ MiguelTip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MiguelTip
+//
+//  Created by Xavier Loera on 8/25/20.
+//  Copyright © 2020 Miguel Gutierrez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
